b111f15cea14dad288d83d54ab45fa4105676dc3
Ahmet Samet GÜLER <ahmetsamet1532@gmail.com>
2025-05-26 01:10:25 +0300

    FENERBAHÇE WON EUROLEAGUE
    !

--- .config/hypr/scripts/idle_inhibit.sh
+++ .config/hypr/scripts/idle_inhibit.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+START_HYPRIDLE_CMD="hypridle &"
+
 while true; do
     status=$(playerctl status 2>/dev/null)
+
     if [[ "$status" == "Playing" ]]; then
-        pkill -STOP hypridle
+        if pgrep -x hypridle > /dev/null; then
+            pkill -x hypridle
+        fi
     else
-        pkill -CONT hypridle
+        if ! pgrep -x hypridle > /dev/null; then
+            eval "$START_HYPRIDLE_CMD"
+        fi
     fi
+
     sleep 5
 done
-
